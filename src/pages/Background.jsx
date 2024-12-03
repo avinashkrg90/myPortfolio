15b61317bc6602542efb14e3644765fd109a7418
Avinash Kumar Gupta <robozealous@gmail.com>
2024-12-03 23:57:44 +0530

    opening page rectified

--- src/pages/Background.jsx
+++ src/pages/Background.jsx
@@ -2,8 +2,22 @@ import React, { Children } from 'react'
 
 const Background = () => {
   return (
-    <div className='fixed inset-0 background -z-50'>
-    </div>
+    <>
+      <div className='fixed inset-0 background -z-50'>
+      </div>
+
+      {/* <div className='text-sky-500 font-extrabold p-20 leading-[16rem] perspective-1000 perspective-origin-center'>
+        <h1 ref={titleRef} className='text-[16rem] backface-hidden transform-3d rotate-x-[90deg] transition-all'>
+          AVINASH<span className='text-black text-[10rem]'>GUPTA</span>
+        </h1>
+      </div> */}
+
+      {/* <div className='text-black text-8xl p-20'>HeroPage</div>
+      <div className='text-yellow-600 text-8xl -bold p-20'>HeroPage</div>
+      <button className='bg-sky-500 p-6 px-8 text-gray-50 text-xl m-10'>Submit</button>
+      <button className='bg-black p-6 px-8 text-gray-50 text-xl m-10'>Submit</button>
+      <button className='bg-yellow-500 p-6 px-8 text-gray-50 text-xl m-10'>Submit</button> */}
+    </>
   )
 }
 
